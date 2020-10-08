e2dc5d97717a220fc55aee44589819fe821a1948
Jack Valley <Juuso.laakso@turku.fi>
2020-10-08 15:33:08 +0300

    Lint, Link and Test improvements (#174)
    Lint commands now actually work.
    
    Footer link now changes from theme package, also made tests for Footer.
    
    Copy to Clipboard is now hidden from aria.

--- src/components/HelFormFields/HelKeywordSelector/HelKeywordSelector.js
+++ src/components/HelFormFields/HelKeywordSelector/HelKeywordSelector.js
@@ -129,7 +129,7 @@ const HelKeywordSelector = ({intl, editor, setDirtyState, setData, currentLocale
                     currentLocale={currentLocale}
                 />
                 <CopyToClipboard text={values['keywords'] ? getKeywordIds(keywords) : ''}>
-                    <button type='button' className="clipboard-copy-button btn btn-default" aria-label={intl.formatMessage({id: 'copy-keyword-to-clipboard'})}>
+                    <button type='button' className="clipboard-copy-button btn btn-default" aria-label={intl.formatMessage({id: 'copy-keyword-to-clipboard'})} aria-hidden='true'>
                         <span className="glyphicon glyphicon-duplicate" aria-hidden="true"></span>
                         <p hidden>duplicate</p>
                     </button>
