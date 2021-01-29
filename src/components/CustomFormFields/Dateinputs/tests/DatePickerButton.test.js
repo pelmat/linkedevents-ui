994ee72f7bb27710b6219713f668ca0d96870764
Jack Valley <Juuso.laakso@turku.fi>
2021-01-29 14:01:47 +0200

    Added tooltips to calendar components
    Should be functional now, but still requires test fixes.

--- src/components/CustomFormFields/Dateinputs/tests/DatePickerButton.test.js
+++ src/components/CustomFormFields/Dateinputs/tests/DatePickerButton.test.js
@@ -4,7 +4,7 @@ import {shallow} from 'enzyme';
 import {IntlProvider} from 'react-intl';
 import mapValues from 'lodash/mapValues';
 import fiMessages from 'src/i18n/fi.json';
-import {Button} from 'reactstrap';
+import {Button, UncontrolledTooltip} from 'reactstrap';
 const testMessages = mapValues(fiMessages, (value, key) => value);
 const intlProvider = new IntlProvider({locale: 'fi', messages: testMessages}, {});
 const {intl} = intlProvider.getChildContext();
@@ -24,11 +24,18 @@ describe('DatePickerButton', () => {
         const wrapper = getWrapper()
         const button = wrapper.find(Button)
         expect(wrapper.find(Button)).toHaveLength(1)
+        expect(button.prop('id')).toBe(defaultProps.id)
         expect(button.prop('disabled')).toBe(defaultProps.disabled)
         expect(button.prop('aria-hidden')).toBe(true)
         expect(button.prop('aria-label')).toBe(intl.formatMessage({id: 'date-picker-button-label'}))
         expect(button.prop('tabIndex')).toBe(-1)
         expect(button.prop('onClick')).toBe(defaultProps.onClick)
         expect(button.prop('className')).toBe('custom-date-input__button glyphicon glyphicon-calendar')
+        const toolTip = wrapper.find(UncontrolledTooltip)
+        expect(wrapper.find(UncontrolledTooltip)).toHaveLength(1)
+        expect(toolTip.prop('placement')).toBe('top')
+        expect(toolTip.prop('target')).toBe(defaultProps.id)
+        expect(toolTip.prop('innerClassName')).toBe('tooltip-disabled')
+        expect(toolTip.prop('hideArrow')).toBeDefined
     })
 })
